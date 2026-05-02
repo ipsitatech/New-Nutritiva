d7ffa98955931c7b6ab59e04c6364e270c280df1
Pranjal <pranjalsarang24@gmail.com>
2026-05-02 13:34:38 +0530

    add trust badges section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-
+import FeaturesSection from './FeaturesSection';
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-    </>
+    <div>
+      <FeaturesSection />
+    </div>
   )
 }
 

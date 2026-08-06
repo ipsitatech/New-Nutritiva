c4aadd91ee00f7c6ff8a3264b336bcf0e87bd116
Ipsita <ipsita@users.noreply.github.com>
2026-08-06 21:17:00 +0530

    Convert header logo click action to react-router-dom Link for smooth SPA navigation (TC02)

--- frontend/src/components/layout/TopBar/NavLogo.jsx
+++ frontend/src/components/layout/TopBar/NavLogo.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../../../assets/images/Nutritiva-logo.png";
 
 export default function NavLogo() {
   return (
-    <a href="/" className="flex items-center gap-2 shrink-0 no-underline">
+    <Link to="/" className="flex items-center gap-2 shrink-0 no-underline">
       <img
         src={logo}
         alt="Nutritva Logo"
         className="h-20 w-20 object-contain"
       />
-    </a>
+    </Link>
   );
 }
